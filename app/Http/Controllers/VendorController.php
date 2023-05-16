b0dee3becf40d504c2ddc7bf79e61a7a494796ea
mjorocio <mj.dejarcio@gmail.com>
2023-05-16 22:34:08 +0800

    Create User and Vendor

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Vendor;
 use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
 
 class VendorController extends Controller
 {
-    //
+
+    public function create(Request $request)
+    {
+        $exist = Vendor::where('vendor_name', $request->name)->where('vendor_code', $request->vendor_code)->exists();
+
+        if ($exist)
+            return new JsonResource([
+                'status' => true,
+                'message' => 'Vendor already exist'
+            ], 409);
+
+        else {
+            $newVendor = Vendor::create($request->all());
+            return $newVendor;
+        }
+    }
 }
