2718cf2d8c7efb574eccaa4a948809e0553da4a3
Emmanuel Paul Moralde <moralde.sama@gmail.com>
2024-06-27 11:59:36 +0800

    Increase Rate Limit to all API endpoints

--- routes/api.php
+++ routes/api.php
@@ -28,93 +28,93 @@ use App\Http\Response\ApiResponse;
 //     return $request->user();
 // });
 
-Route::post('/login', [UserController::class, 'login'])->name('login');
-Route::post('/logout', [UserController::class, 'logout'])->middleware('auth:api');
-Route::post('/position', [GpsController::class, 'sendGPS']);
+Route::post('/login', [UserController::class, 'login'])->name('login')->middleware('throttle:60,1');
+Route::post('/logout', [UserController::class, 'logout'])->middleware(['auth:api', 'throttle:60,1']);
+Route::post('/position', [GpsController::class, 'sendGPS'])->middleware('throttle:1000,1');
 Route::post('/check-server', [GpsController::class, 'checkServer']);
-Route::post('/vendor/create-with-account', [TransporterController::class, 'publicCreate']);
-Route::post('/user/publicRegister', [UserController::class, 'publicRegister']);
+Route::post('/vendor/create-with-account', [TransporterController::class, 'publicCreate'])->middleware('throttle:60,1');;
+Route::post('/user/publicRegister', [UserController::class, 'publicRegister'])->middleware('throttle:60,1');;
 
 Route::group([
     'middleware' => 'auth:api',
     'prefix' => 'user'
 ], function(){
-    Route::post('/create', [UserController::class, 'register']);
-    Route::post('/list', [UserController::class, 'list']);
-    Route::get('/userById/{id}', [UserController::class, 'userById']);
-    Route::put('/update/{id}', [UserController::class, 'update']);
-    Route::put('/updatePassword/{id}', [UserController::class, 'updatePassword']);
-    Route::put('/resetPassword/{id}', [UserController::class, 'resetPassword']);
+    Route::post('/create', [UserController::class, 'register'])->middleware('throttle:60,1');
+    Route::post('/list', [UserController::class, 'list'])->middleware('throttle:200,1');
+    Route::get('/userById/{id}', [UserController::class, 'userById'])->middleware('throttle:60,1');
+    Route::put('/update/{id}', [UserController::class, 'update'])->middleware('throttle:60,1');
+    Route::put('/updatePassword/{id}', [UserController::class, 'updatePassword'])->middleware('throttle:60,1');
+    Route::put('/resetPassword/{id}', [UserController::class, 'resetPassword'])->middleware('throttle:60,1');
 });
 
 Route::group([
     'middleware' => 'auth:api',
     'prefix' => 'vendor'  // Transporter === Vendor
 ], function(){
-    Route::get('/list', [TransporterController::class, 'list']);
-    Route::get('/vendorById/{id}', [TransporterController::class, 'transporterById']);
-    Route::put('/update/{id}', [TransporterController::class, 'update']);
-    Route::delete('/delete/{id}', [TransporterController::class, 'delete']);
-    Route::post('/create', [TransporterController::class, 'create']);
+    Route::get('/list', [TransporterController::class, 'list'])->middleware('throttle:200,1');;
+    Route::get('/vendorById/{id}', [TransporterController::class, 'transporterById'])->middleware('throttle:60,1');
+    Route::put('/update/{id}', [TransporterController::class, 'update'])->middleware('throttle:60,1');
+    Route::delete('/delete/{id}', [TransporterController::class, 'delete'])->middleware('throttle:60,1');
+    Route::post('/create', [TransporterController::class, 'create'])->middleware('throttle:60,1');
 });
 
 Route::group([
     'middleware' => 'auth:api',
     'prefix' => 'vehicle'
 ], function(){
-    Route::post('/create', [VehicleController::class, 'create']);
-    Route::post('/create-complete-info', [VehicleController::class, 'createCompleteData']);
-    Route::post('/list', [VehicleController::class, 'list']);
-    Route::get('/vehicleById/{id}', [VehicleController::class, 'vehicleById']);
-    Route::post('/export', [VehicleController::class, 'vehicleExport']);
-    Route::post('/provisioning/export', [VehicleController::class, 'provisioningExport']);
-    Route::post('/unregistered/export', [VehicleController::class, 'unregisteredExport']);
-    Route::put('/update/{id}', [VehicleController::class, 'update']);
-    Route::put('/massUpdate', [VehicleController::class, 'massUpdate']);
-    Route::delete('/delete/{id}', [VehicleController::class, 'delete']);
+    Route::post('/create', [VehicleController::class, 'create'])->middleware('throttle:60,1');
+    Route::post('/create-complete-info', [VehicleController::class, 'createCompleteData'])->middleware('throttle:60,1');
+    Route::post('/list', [VehicleController::class, 'list'])->middleware('throttle:200,1');
+    Route::get('/vehicleById/{id}', [VehicleController::class, 'vehicleById'])->middleware('throttle:200,1');
+    Route::post('/export', [VehicleController::class, 'vehicleExport'])->middleware('throttle:60,1');
+    Route::post('/provisioning/export', [VehicleController::class, 'provisioningExport'])->middleware('throttle:60,1');
+    Route::post('/unregistered/export', [VehicleController::class, 'unregisteredExport'])->middleware('throttle:60,1');
+    Route::put('/update/{id}', [VehicleController::class, 'update'])->middleware('throttle:60,1');
+    Route::put('/massUpdate', [VehicleController::class, 'massUpdate'])->middleware('throttle:60,1');
+    Route::delete('/delete/{id}', [VehicleController::class, 'delete'])->middleware('throttle:60,1');
 });
 
 Route::group([
     'middleware' => 'auth:api',
     'prefix' => 'customer'  // Transporter === Vendor
 ], function(){
-    Route::post('/create', [CustomerController::class, 'create']);
-    Route::get('/customerById/{id}', [CustomerController::class, 'customerById']);
-    Route::post('/list', [CustomerController::class, 'list']);
-    Route::put('/update/{id}', [CustomerController::class, 'update']);
-    Route::delete('/delete/{id}', [CustomerController::class, 'delete']);
+    Route::post('/create', [CustomerController::class, 'create'])->middleware('throttle:60,1');
+    Route::get('/customerById/{id}', [CustomerController::class, 'customerById'])->middleware('throttle:60,1');
+    Route::post('/list', [CustomerController::class, 'list'])->middleware('throttle:200,1');;
+    Route::put('/update/{id}', [CustomerController::class, 'update'])->middleware('throttle:60,1');
+    Route::delete('/delete/{id}', [CustomerController::class, 'delete'])->middleware('throttle:60,1');
 
     // For Customer-IP-Port
-    Route::post('/ip-port/create', [CustomerIpPortController::class, 'create']);
-    Route::get('/ip-port/ipPortById/{id}', [CustomerIpPortController::class, 'ipPortById']);
-    Route::post('/ip-port/list', [CustomerIpPortController::class, 'list']);
-    Route::put('/ip-port/update/{id}', [CustomerIpPortController::class, 'update']);
-    Route::delete('/ip-port/delete/{id}', [CustomerIpPortController::class, 'delete']);
+    Route::post('/ip-port/create', [CustomerIpPortController::class, 'create'])->middleware('throttle:60,1');
+    Route::get('/ip-port/ipPortById/{id}', [CustomerIpPortController::class, 'ipPortById'])->middleware('throttle:60,1');
+    Route::post('/ip-port/list', [CustomerIpPortController::class, 'list'])->middleware('throttle:200,1');;
+    Route::put('/ip-port/update/{id}', [CustomerIpPortController::class, 'update'])->middleware('throttle:60,1');
+    Route::delete('/ip-port/delete/{id}', [CustomerIpPortController::class, 'delete'])->middleware('throttle:60,1');
 });
 
 Route::group([
     'middleware' => 'auth:api',
     'prefix' => 'current'
 ], function(){
-    Route::post('/create', [CurrentCustomerController::class, 'create']);
-    Route::post('/list', [CurrentCustomerController::class, 'list']);
-    Route::get('/currentCustById/{id}', [CurrentCustomerController::class, 'currentCustById']);
-    Route::put('/update/{id}', [CurrentCustomerController::class, 'update']);
-    Route::delete('/delete/{id}', [CurrentCustomerController::class, 'delete']);
+    Route::post('/create', [CurrentCustomerController::class, 'create'])->middleware('throttle:60,1');
+    Route::post('/list', [CurrentCustomerController::class, 'list'])->middleware('throttle:200,1');;
+    Route::get('/currentCustById/{id}', [CurrentCustomerController::class, 'currentCustById'])->middleware('throttle:60,1');
+    Route::put('/update/{id}', [CurrentCustomerController::class, 'update'])->middleware('throttle:60,1');
+    Route::delete('/delete/{id}', [CurrentCustomerController::class, 'delete'])->middleware('throttle:60,1');
 });
 
 Route::group([
     'middleware' => 'auth:api',
     'prefix' => 'assignment'
 ], function(){
-    Route::post('/create', [VehicleAssignmentsController::class, 'create']);
-    Route::post('/list', [VehicleAssignmentsController::class, 'list']);
-    Route::get('/assignmentById/{id}', [VehicleAssignmentsController::class, 'assignmentById']);
-    Route::put('/update/{id}', [VehicleAssignmentsController::class, 'update']);
-    Route::put('/update-assign-customer/{id}', [VehicleAssignmentsController::class, 'updateAssignmentCustomer']);
-    Route::delete('/delete/{id}', [VehicleAssignmentsController::class, 'delete']);
-    Route::put('/approve/{id}', [VehicleAssignmentsController::class, 'approve']);
-    Route::put('/reject/{id}', [VehicleAssignmentsController::class, 'reject']);
+    Route::post('/create', [VehicleAssignmentsController::class, 'create'])->middleware('throttle:60,1');
+    Route::post('/list', [VehicleAssignmentsController::class, 'list'])->middleware('throttle:200,1');;
+    Route::get('/assignmentById/{id}', [VehicleAssignmentsController::class, 'assignmentById'])->middleware('throttle:60,1');
+    Route::put('/update/{id}', [VehicleAssignmentsController::class, 'update'])->middleware('throttle:60,1');
+    Route::put('/update-assign-customer/{id}', [VehicleAssignmentsController::class, 'updateAssignmentCustomer'])->middleware('throttle:60,1');
+    Route::delete('/delete/{id}', [VehicleAssignmentsController::class, 'delete'])->middleware('throttle:250,1');
+    Route::put('/approve/{id}', [VehicleAssignmentsController::class, 'approve'])->middleware('throttle:250,1');
+    Route::put('/reject/{id}', [VehicleAssignmentsController::class, 'reject'])->middleware('throttle:250,1');
 });
 
 //This will catch GET request to /api/register but  PUT,DELETE, OPTIONS etc. fails
