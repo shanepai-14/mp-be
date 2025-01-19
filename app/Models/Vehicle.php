3424aa08f85f84e8fded67bea988ec083165f278
mong benitez <mong.benitez@gmail.com>
2025-01-19 15:43:40 +0800

    remove driver name as fillable in  vehicle model

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -104,7 +104,7 @@ class Vehicle extends Model
         'device_id_plate_no',
         'transporter_id',
         // 'vehicle_status',
-        'driver_name',
+        // 'driver_name',
         // 'mileage',
         'register_by_user_id',
         'updated_by_user_id'
